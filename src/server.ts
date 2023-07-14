02cbdc4a5e31e6aebc710502865232bf7302bc48
Hyounsuk Kim <kkoor586@naver.com>
2023-07-15 04:18:06 +0900

    -: Add cookie-parser, @types/cookie-parser

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import api from "./api/api";
+import cookieParser from "cookie-parser";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api", api);
 
