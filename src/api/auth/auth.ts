80af9964bd96edd9ecc4c5d1061d2e0974934a1a
Hyounsuk Kim <kkoor586@naver.com>
2023-07-15 04:14:19 +0900

    Feat: Auth API (로그인, 로그아웃)

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -1,10 +1,24 @@
 import express, { Router } from "express";
-import { checkEmail, checkNickname, localRegester } from "./authController";
+import {
+  accesstoken,
+  checkEmail,
+  checkNickname,
+  localRegester,
+  loginLocal,
+  loginSuccess,
+  logout,
+  refreshtoken,
+} from "./authController";
 
 const auth: Router = express.Router();
 
 auth.route("/register/local").post(localRegester);
 auth.route("/register/local/check-email").get(checkEmail);
 auth.route("/register/local/check-nickname").get(checkNickname);
+auth.route("/login/local").post(loginLocal);
+auth.route("/login/success").get(loginSuccess);
+auth.route("/accesstoken").get(accesstoken);
+auth.route("/refreshtoken").get(refreshtoken);
+auth.route("/logout").post(logout);
 
 export default auth;
--- src/api/auth/authController.ts
+++ src/api/auth/authController.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import User from "../../models/User";
+import bcrypt from "bcrypt";
 
-// 회원가입
+/* <-- 회원가입 --> */
 export const localRegester = async (req: Request, res: Response) => {
   const { email, name, nickname, password, confirmPassword } = req.body;
   const exists = await User.exists({ $or: [{ email }, { nickname }] });
@@ -40,7 +41,7 @@ export const localRegester = async (req: Request, res: Response) => {
   }
 };
 
-// 이메일 중복확인
+/* <-- 이메일 중복확인 --> */
 export const checkEmail = async (req: Request, res: Response) => {
   const { email } = req.body;
   const exists = await User.exists({ email });
@@ -68,7 +69,7 @@ export const checkEmail = async (req: Request, res: Response) => {
   }
 };
 
-// 닉네임 중복확인
+/* <-- 닉네임 중복확인 --> */
 export const checkNickname = async (req: Request, res: Response) => {
   const { nickname } = req.body;
   const exists = await User.exists({ nickname });
@@ -95,3 +96,54 @@ export const checkNickname = async (req: Request, res: Response) => {
     }
   }
 };
+
+/* <-- 로그인 --> */
+export const loginLocal = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).populate("studylogs");
+
+  try {
+    if (!user) {
+      throw new Error(
+        "가입된 계정이 존재하지 않습니다. Email 정보를 확인하세요."
+      );
+    } else {
+      const pass = await bcrypt.compare(password, user.password);
+      if (!pass) {
+        throw new Error(
+          "Email 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요."
+        );
+      }
+
+      // jwt 인증 부분
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(403).send({
+        isSuccess: false,
+        message: error.message,
+      });
+    } else {
+      return res.status(403).send({
+        isSuccess: false,
+        message: String(error),
+      });
+    }
+  }
+};
+
+// 엑세스 토큰
+export const accesstoken = (req: Request, res: Response) => {};
+
+// 리프레시 토큰
+export const refreshtoken = (req: Request, res: Response) => {};
+
+// 로그인한 사용자 정보 전달
+export const loginSuccess = (req: Request, res: Response) => {};
+
+/* <-- 로그아웃 --> */
+export const logout = (req: Request, res: Response) => {};
