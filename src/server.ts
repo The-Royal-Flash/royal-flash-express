1969f38075cba6ebd32ef272541bea8bd5cb9a76
Hyounsuk Kim <kkoor586@naver.com>
2023-07-15 04:20:38 +0900

    -: Add cors, @types/cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import api from "./api/api";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.use("/api", api);
 
