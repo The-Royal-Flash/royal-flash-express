ebb8a28fa2404e78b3667c7cb3878c239581b830
Hyounsuk Kim <kkoor586@naver.com>
2023-07-18 00:54:27 +0900

    Feat: Quizlet API (학습세트 삭제)

--- src/api/controllers/quizlet.controller.ts
+++ src/api/controllers/quizlet.controller.ts
@@ -35,28 +35,22 @@ export const createQuizlet = async (req: Request, res: Response) => {
     }
 
     // Question Card 생성
-    questionCardList.forEach(
-      async (questionCard: {
-        question: string;
-        link: string;
-        answer: string;
-      }) => {
-        const newQuestionCard = await QuestionCard.create({
-          question: questionCard.question,
-          answer: questionCard.answer,
-          link: questionCard.link,
+    for (const questionCard of questionCardList) {
+      const newQuestionCard = await QuestionCard.create({
+        question: questionCard.question,
+        answer: questionCard.answer,
+        link: questionCard.link,
+      });
+      if (!newQuestionCard) {
+        return res.status(400).send({
+          isSuccess: false,
+          message: "QuestionCard creation error",
         });
-        if (!newQuestionCard) {
-          return res.status(400).send({
-            isSuccess: false,
-            message: "QuestionCard creation error",
-          });
-        }
-
-        // 생성된 Quizlet의 Question Card List에 Question Card 추가
-        quizlet.questionCardList.push(newQuestionCard._id);
       }
-    );
+
+      // 생성된 Quizlet의 Question Card List에 Question Card 추가
+      quizlet.questionCardList.push(newQuestionCard._id);
+    }
 
     // Quizlet 저장
     quizlet.save();
@@ -65,6 +59,71 @@ export const createQuizlet = async (req: Request, res: Response) => {
     return res.status(200).send({
       isSuccess: true,
       message: "Creation successful",
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).send({
+        isSuccess: false,
+        message: error.message,
+      });
+    } else {
+      return res.status(400).send({
+        isSuccess: false,
+        message: String(error),
+      });
+    }
+  }
+};
+
+/* <-- 학습세트 삭제 --> */
+export const deleteQuizlet = async (req: Request, res: Response) => {
+  const { quizletId } = req.params;
+  const { id } = (req as any).user;
+
+  try {
+    // DB에서 Quizlet 호출
+    const quizlet = await Quizlet.findById(quizletId);
+    if (!quizlet) {
+      return res.status(400).send({
+        isSuccess: false,
+        message: "Quizlet not found",
+      });
+    }
+
+    // 검색된 Quizlet 소유권이 로그인한 사용자와 다를 경우
+    if (String(quizlet.owner) !== String(id)) {
+      return res.status(400).send({
+        isSuccess: false,
+        message: "Permission error",
+      });
+    }
+
+    // Quizlet이 소유한 Question Card 삭제
+    for (const questionCard of quizlet.questionCardList) {
+      const deletedQuestionCard = await QuestionCard.deleteOne({
+        _id: questionCard,
+      });
+      if (!deletedQuestionCard) {
+        return res.status(400).send({
+          isSuccess: false,
+          message: "Question card deleting error",
+        });
+      }
+    }
+
+    // Quizlet 삭제
+    const deletedQuizlet = await Quizlet.deleteOne({ _id: quizlet._id });
+    if (!deletedQuizlet) {
+      return res.status(400).send({
+        isSuccess: false,
+        message: "Quizlet deleting error",
+      });
+    }
+
+    // 삭제 성공 반환
+    return res.status(200).send({
+      isSuccess: true,
+      message: "Quizlet deleted successful",
     });
   } catch (error) {
     if (error instanceof Error) {
--- src/api/routes/quizlet.ts
+++ src/api/routes/quizlet.ts
@@ -1,9 +1,16 @@
 import express, { Router } from "express";
-import { createQuizlet } from "../controllers/quizlet.controller";
+import {
+  createQuizlet,
+  deleteQuizlet,
+} from "../controllers/quizlet.controller";
 import authTokenMiddleware from "../../middlewares/authTokenMiddleware";
 
 const quizlet: Router = express.Router();
 
 quizlet.route("/create").all(authTokenMiddleware).post(createQuizlet);
+quizlet
+  .route("/delete/:quizletId")
+  .all(authTokenMiddleware)
+  .delete(deleteQuizlet);
 
 export default quizlet;
