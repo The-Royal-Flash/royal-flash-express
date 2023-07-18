799b9b03f096b99eb46bc3a83018fe1afa806a21
Hyounsuk Kim <kkoor586@naver.com>
2023-07-19 00:04:06 +0900

    Feat: Quizlet API - 학습세트 수정

--- src/api/routes/quizlet.ts
+++ src/api/routes/quizlet.ts
@@ -2,12 +2,14 @@ import express, { Router } from "express";
 import {
   createQuizlet,
   deleteQuizlet,
+  editQuizlet,
 } from "../controllers/quizlet.controller";
 import authTokenMiddleware from "../../middlewares/authTokenMiddleware";
 
 const quizlet: Router = express.Router();
 
 quizlet.route("/create").all(authTokenMiddleware).post(createQuizlet);
+quizlet.route("/edit/:quizletId").all(authTokenMiddleware).post(editQuizlet);
 quizlet
   .route("/delete/:quizletId")
   .all(authTokenMiddleware)
