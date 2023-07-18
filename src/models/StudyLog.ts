ed2a01fefcfdf7e0f77c9f3d1b54d317fde0eb5f
Hyounsuk Kim <kkoor586@naver.com>
2023-07-18 20:42:17 +0900

    Refac: DB Models - 학습기록

--- src/models/StudyLog.ts
+++ src/models/StudyLog.ts
@@ -5,6 +5,7 @@ interface IStudyLog {
   correctList: Types.ObjectId[];
   createAt: Date;
   owner: Types.ObjectId;
+  about: Types.ObjectId;
 }
 
 const studyLogSchema = new mongoose.Schema<IStudyLog>({
@@ -12,6 +13,7 @@ const studyLogSchema = new mongoose.Schema<IStudyLog>({
   correctList: [{ type: mongoose.Schema.Types.ObjectId, ref: "QuestionCard" }],
   createAt: { type: Date, default: Date.now },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  about: { type: mongoose.Schema.Types.ObjectId, ref: "Quizlet" },
 });
 
 const StudyLog = mongoose.model<IStudyLog>("StudyLog", studyLogSchema);
