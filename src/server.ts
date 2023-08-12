f8b90d89476511aea58d63fe70308a5e59c73980
Hyounsuk Kim <kkoor586@naver.com>
2023-08-13 04:09:19 +0900

    Fix: server - cors 에러 수정

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-	cors({
-		origin: '*',
-		methods: ['GET', 'POST', 'DELETE', 'UPDATE'],
-		credentials: true,
-	}),
-);
+app.use(cors());
 
 app.use('/uploads', express.static('uploads'));
 app.use('/api', api);
