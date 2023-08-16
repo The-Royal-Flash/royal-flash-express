a777790cd29d83edbf0c1773e3c70a110982ff6b
Hyounsuk Kim <kkoor586@naver.com>
2023-08-16 22:23:41 +0900

    Feat: search.controller - 생성한 학습세트 검색

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: 'https://web-royal-flash-react-3prof2llkv7xq3p.sel4.cloudtype.app',
+		origin: [
+			'https://web-royal-flash-react-3prof2llkv7xq3p.sel4.cloudtype.app',
+		],
 		methods: ['GET', 'POST'],
 		credentials: true,
 	}),
